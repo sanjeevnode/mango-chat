b32d3eae53e965b834f342e9193a283fd1c09985
sanjeevnode <me.sanjeevks@gmail.com>
2026-01-03 02:49:16 +0530

    Refactor response handling in user controller and update AppResponse to use JSONResponse

--- app/controller/user_controller.py
+++ app/controller/user_controller.py
@@ -3,22 +3,23 @@ from app.models.user import User
 from app.schemas.response_schema import AppResponse
 from app.schemas.user_schema import UserResponse, UserRequest
 import bcrypt
+from fastapi import status
 
 
 async def get_user(db: Session):
     try:
         users = db.query(User).all()
-        user_res = [UserResponse.model_validate(user).model_dump() for user in users]
-        return AppResponse(status=200, data=user_res,message="Users retrieved successfully").to_dict()
+        user_res = [UserResponse.model_validate(user).model_dump(mode='json') for user in users]
+        return AppResponse(status=status.HTTP_200_OK, data=user_res, message="Users retrieved successfully").send()
     except Exception as e:
-        return AppResponse(status=500, message=str(e)).to_dict()
+        return AppResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR, message=str(e)).send()
     
     
 async def create_user(user_data: UserRequest, db: Session):
     try:
         user_exists = db.query(User).filter(User.username==user_data.username).first()
         if user_exists:
-            return AppResponse(status=400, message="Username already exists").to_dict()
+            return AppResponse(status=status.HTTP_400_BAD_REQUEST, message="Username already exists").send()
         # Hash the password
         hashed_password = bcrypt.hashpw(user_data.password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
         
@@ -32,20 +33,18 @@ async def create_user(user_data: UserRequest, db: Session):
         db.commit()
         db.refresh(new_user)
         
-        return AppResponse(status=201, data=UserResponse.model_validate(new_user).model_dump(), message="User created successfully").to_dict()
+        return AppResponse(status=status.HTTP_201_CREATED, data=UserResponse.model_validate(new_user).model_dump(mode='json'), message="User created successfully").send()
     except Exception as e:
         db.rollback()
-        return AppResponse(status=500, message=str(e)).to_dict()
-    except Exception as e:
-        return AppResponse(status=500, message=str(e)).to_dict()
+        return AppResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR, message=str(e)).send()
     
     
 async def authenticate_user(username: str, password: str, db: Session):
     try:
         user = db.query(User).filter(User.username==username).first()
         if user and bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
-            return AppResponse(status=200, data=UserResponse.model_validate(user).model_dump(), message="Authentication successful").to_dict()
+            return AppResponse(status=status.HTTP_200_OK, data=UserResponse.model_validate(user).model_dump(mode='json'), message="Authentication successful").send()
         else:
-            return AppResponse(status=401, message="Invalid username or password").to_dict()
+            return AppResponse(status=status.HTTP_401_UNAUTHORIZED, message="Invalid username or password").send()
     except Exception as e:
-        return AppResponse(status=500, message=str(e)).to_dict()
+        return AppResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR, message=str(e)).send()
--- app/schemas/response_schema.py
+++ app/schemas/response_schema.py
@@ -1,12 +1,18 @@
+from fastapi.responses import JSONResponse
+
+
 class AppResponse:
-    def __init__(self, status: int, data: object = None, message: str =None):
+    def __init__(self, status: int, data: object = None, message: str = None):
         self.status = status
         self.data = data
         self.message = message
 
-    def to_dict(self):
-        return {
-            "status": self.status,
-            "data": self.data,
-            "message": self.message,
-        }
+    def send(self):
+        return JSONResponse(
+            status_code=self.status,
+            content={
+                "status": self.status,
+                "data": self.data,
+                "message": self.message,
+            }
+        )
