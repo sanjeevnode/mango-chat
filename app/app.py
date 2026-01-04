3bbb4b19dffd4c9d56daae87c86bab33e6faed8b
sanjeevnode <me.sanjeevks@gmail.com>
2026-01-04 11:42:57 +0530

    Add CORS middleware configuration for frontend access

--- app/app.py
+++ app/app.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.router.user_router import user_router
 from app.router.auth_router import auth_router
 from app.router.channel_router import channel_router
 from app.router.message_router import message_router
-
+import os
 
 app = FastAPI(
     title="Mango Chat 🥭",
     description="Peer to peer chat application",
+)
+
+Allowed_frontend = os.getenv("FRONTEND_HOST", "*")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[Allowed_frontend],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router)
